f0fe8d8b9980b6f064a368d867258915e80d29e5
Amol Tathe <amoltathe8080@gmail.com>
2026-09-24 16:21:06 +0530

    Create customers table

--- database/01_create_tables.sql
+++ database/01_create_tables.sql
@@ -9,3 +9,17 @@ CREATE TABLE brokers (
     phone VARCHAR2(20),
     status VARCHAR2(20) DEFAULT 'ACTIVE'
 );
+
+
+-- ============================================
+-- CUSTOMERS TABLE
+-- ============================================
+
+CREATE TABLE customers (
+    customer_id NUMBER PRIMARY KEY,
+    customer_name VARCHAR2(100) NOT NULL,
+    email VARCHAR2(150) UNIQUE,
+    phone VARCHAR2(20),
+    address VARCHAR2(250),
+    status VARCHAR2(20) DEFAULT 'ACTIVE'
+);
